7a237fb1df8f6a5498fda02ec84451d1b4d4776b
Brandon Barker <bbarker.py@protonmail.ch>
2024-05-28 10:57:44 -0600

    fmt: tst

--- tst/unit/fixed_point/funcs.hpp
+++ tst/unit/fixed_point/funcs.hpp
@@ -4,8 +4,6 @@
 #include <math.h>
 
 // 0.607102
-double func( double x ) {
-  return ( 1.0 + std::cos( x ) ) / 3.0;
-}
+double func( double x ) { return ( 1.0 + std::cos( x ) ) / 3.0; }
 
 #endif // FUNCS_HPP_
--- tst/unit/fixed_point/test_fp.cpp
+++ tst/unit/fixed_point/test_fp.cpp
@@ -10,8 +10,8 @@
 
 TEST_CASE( "Fixed Point", "Solvers" ) {
   const double truth = 0.607102;
-  const double x0 = 0.5;
-  double ans = FixedPointSolve( func, x0 );
-  std::printf("%f\n", ans);
+  const double x0    = 0.5;
+  double ans         = FixedPointSolve( func, x0 );
+  std::printf( "%f\n", ans );
   REQUIRE( SoftEqual( ans, truth, 1.0e-5 ) );
 }
