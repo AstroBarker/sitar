6cbda203c487106d4be5dda4cf42edf4b2f04164
Brandon Barker <bbarker.py@protonmail.ch>
2024-07-26 20:31:13 -0400

    Fix: issue in AA when first jumpstart iteration converges (#8)
    fix: issue in FPAA when first FP iteration converges, the following AA iterations diverge

--- src/solve.hpp
+++ src/solve.hpp
@@ -2,7 +2,6 @@
  * File    : solve.hpp
  * Author  : Brandon Barker
  * Purpose : Implement Anderson-accelerated fixed point iteration
- * TODO: Try the Newton...
  **/
 
 #include <cmath>
@@ -132,6 +131,7 @@ T Newton( F target, F dTarget, T x0, Args... args ) {
       printf( " %d %e %e \n", n, xn, error );
     }
   }
+  std::printf( "%d \n", n );
   return x0;
 }
 
